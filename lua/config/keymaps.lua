32de0bc0803f8d8f29ab91ceed68b275f2e89c93
Mark Ryall <mark@ryall.name>
2025-04-26 12:21:49 +1000

    upd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,25 +3,25 @@
 -- Add any additional keymaps here
 vim.keymap.set(
   "n",
-  "<Leader>[p",
+  "<Leader>op",
   ':let @+ = expand("%")<CR>',
   { silent = true, noremap = true, desc = "copy file path" }
 )
 vim.keymap.set(
   "n",
-  "<Leader>[P",
+  "<Leader>oP",
   ':let @+ = expand("%:p")<CR>',
   { silent = true, noremap = true, desc = "copy full file path" }
 )
 vim.keymap.set(
   "n",
-  "<Leader>[l",
+  "<Leader>ol",
   ':let @+ = expand("%") . ":" . line(".")<CR>',
   { silent = true, noremap = true, desc = "copy file path and line number" }
 )
 vim.keymap.set(
   "n",
-  "<Leader>[L",
+  "<Leader>oL",
   ':let @+ = expand("%:p") . ":" . line(".")<CR>',
   { silent = true, noremap = true, desc = "copy file path and line number" }
 )
